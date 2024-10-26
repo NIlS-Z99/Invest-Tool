be21f90489389af58ebba15b4bb26188be251afd
Nils-Treuheit <75490938+Nils-Treuheit@users.noreply.github.com>
2024-10-26 20:19:43 +0200

    improve amumbo tool

--- amumbo_strat_helper.py
+++ amumbo_strat_helper.py
@@ -12,13 +12,14 @@ from sys import argv
 VIX_THRESHOLD = 28  # Define high volatility as VIX > 28
 SMA_PERIOD = 50     # For quarter 200 moving average
 SP500_SMA_COLORS = ["red",
-                    "mediumorchid",
+                    "blueviolet",
                     "lightcoral",
-                    "sandybrown",
+                    "plum",
                     "deeppink",
                     "maroon"]
-VIX_SMA_COLORS = ["yellowgreen",
-                  "orange"]
+VIX_M75_COLORS = ["gold",
+                  "orange",
+                  "chocolate"]
 
 
 
@@ -38,16 +39,22 @@ def calculate_sma(data, smaPeriod):
     return data
 def calculate_sma200(data): return calculate_sma(data, 200)
 
+def calculate_m75(data, smaPeriod):
+    data[f'M75_{smaPeriod}'] = data['Close'].rolling(window=smaPeriod).apply(lambda x: np.percentile(x, 75), raw=True)
+    return data
+
 # Function to check for warning signals
 def check_signals(sp500_data, vix_data, vix_thresh, sma_period):
     # Calculate SMA for VIX
-    vix_data = calculate_sma(vix_data, 14)
-    vix_data = calculate_sma(vix_data, 30)
+    vix_data = calculate_m75(vix_data, 7)
+    vix_data = calculate_m75(vix_data, 14)
+    vix_data = calculate_m75(vix_data, 30)
 
     # Latest VIX value and short period SMA
     latest_vix = vix_data['Close'].iloc[-1].tolist() #[0]
-    latest_vix_sma14 = vix_data['SMA14'].iloc[-1].tolist()
-    latest_vix_sma30 = vix_data['SMA30'].iloc[-1].tolist()
+    latest_vix_m75_7 = vix_data['M75_7'].iloc[-1].tolist()
+    latest_vix_m75_14 = vix_data['M75_14'].iloc[-1].tolist()
+    latest_vix_m75_30 = vix_data['M75_30'].iloc[-1].tolist()
     
     # Calculate SMA200 for S&P 500
     sp500_data = calculate_sma200(sp500_data)
@@ -83,8 +90,8 @@ def check_signals(sp500_data, vix_data, vix_thresh, sma_period):
     print("{:24s} {:>9.4f}\n".format("Latest S&P 500 SMA200:",round(latest_sma200,4)))
     
     # Conditions
-    high_volatility = (latest_vix > vix_thresh) or \
-        (latest_vix_sma14 > vix_thresh*0.9) or (latest_vix_sma30 > vix_thresh*0.75) 
+    high_volatility = (latest_vix > vix_thresh) or (latest_vix_m75_7 > vix_thresh*0.9) or\
+        (latest_vix_m75_14 > vix_thresh*0.8) or (latest_vix_m75_30 > vix_thresh*0.75) 
     below_sma200 = latest_price < latest_sma200
     below_sma150 = latest_price < latest_sma150
     below_smaq = latest_price < latest_smaq
@@ -119,7 +126,8 @@ def plot_scraped_data(sp500_data, vix_data, vix_thresh, sma_period):
 
     # Plot S&P 500 and its SMA200 on the same plot
     ax1.plot(sp500_data.index, sp500_data['Close'], label='S&P 500 Close', color='blue', alpha=0.6)
-    ax1.fill_between(sp500_data.index, np.zeros_like(sp500_data['Close'])+min(sp500_data['Close']), sp500_data['Close'],
+    scale = np.array(range(int(500*round(min(sp500_data['Close'])/500))-500, int(max(sp500_data['Close']))+500, 500))
+    ax1.fill_between(sp500_data.index, np.zeros_like(sp500_data['Close'])+min(scale), sp500_data['Close'],
                      #np.zeros_like(sp500_data['Close',"^GSPC"])+min(sp500_data['Close',"^GSPC"]), 
                      #sp500_data['Close',"^GSPC"], 
                      color='cyan', alpha=0.3)
@@ -131,27 +139,35 @@ def plot_scraped_data(sp500_data, vix_data, vix_thresh, sma_period):
                                        ('150' in sma) else 'dotted'))
             count += 1
     ax1.set_xlabel('Date')
-    ax1.set_ylabel('S&P 500 Price', color='blue')
+    ax1.set_ylabel('S&P 500 Index', color='blue')
     ax1.tick_params(axis='y', labelcolor='blue')
+    ax1.set_yticks(scale)
     ax1.legend(loc='upper left')
 
     # Create a second y-axis for VIX
     ax2 = ax1.twinx()
     ax2.plot(vix_data.index, vix_data['Close'], label='VIX Close', color='darkgreen', alpha=0.6)
     count = 0
-    for sma in vix_data.columns: #.get_level_values('Price'):
-        if 'SMA' in sma: 
-            ax2.plot(vix_data.index, vix_data[sma], label=f'VIX {sma}', color=VIX_SMA_COLORS[count], linestyle='--')
+    for m75 in vix_data.columns: #.get_level_values('Price'):
+        if 'M75_' in m75: 
+            ax2.plot(vix_data.index, vix_data[m75], label=f'VIX {m75}', color=VIX_M75_COLORS[count], linestyle='--')
             count += 1
-    ax2.axhline(y=vix_thresh, color='black', linestyle='solid', label='VIX Threshold')
+    ax2.axhline(y=vix_thresh, color='black', linestyle='-.', label='VIX Threshold')
+    ax2.fill_between(vix_data.index, np.zeros_like(vix_data['Close'])+min(vix_data['Close']), vix_data['Close'],
+                     #np.zeros_like(vix_data['Close',"^VIX"])+min(vix_data['Close',"^VIX"]), 
+                     #vix_data['Close',"^VIX"], 
+                     color='lime', alpha=0.3)
     #ax2.fill_between(vix_data[vix_data['Close',"^VIX"]>vix_thresh].index, np.zeros_like(vix_data[vix_data['Close',"^VIX"]>vix_thresh]['Close',"^VIX"])+vix_thresh, vix_data[vix_data['Close',"^VIX"]>vix_thresh]['Close',"^VIX"], color='crimson', alpha=0.3)
     ax2.set_ylabel('VIX Index', color='green')
     ax2.tick_params(axis='y', labelcolor='green')
-    ax2.legend(loc='lower right')
+    ax2.legend(loc='upper center')
+    scale = np.array(range(int(5*round(min(vix_data['Close'])/5)), int(max(max(vix_data['Close']),vix_thresh*2+1)), 5))
+    scale = np.insert(scale, np.where(scale == 5*(round(vix_thresh/5)+1))[0], vix_thresh)
+    ax2.set_yticks(scale)
 
     # Title and grid
-    plt.title('S&P 500 Price, SMA200 and VIX Index Over the Last 2 Years')
-    plt.grid(True)
+    plt.title('S&P 500 Index and VIX Index (Last 2 Years)')
+    #plt.grid(True)
     plt.show()
 
 
